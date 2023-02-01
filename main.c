cabdcd1bb152b35e2359638182fd15540fd2bcb3
BRTEK007 <bartoszkonderla@gmail.com>
2023-02-01 17:12:52 +0100

    working tree

--- main.c
+++ main.c
@@ -6,17 +6,41 @@
 #define CVECTOR_LOGARITHMIC_GROWTH
 #include "c-vector/cvector.h"
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0') 
+typedef struct bit_code_t{
+  unsigned char code;
+  int len;
+} bit_code_t;
 
+void bit_code_print(bit_code_t bc){
+  int mask = 1u;
+  char str[bc.len + 1];
+  for(int i = 0; i < bc.len; i++){
+    str[i] = mask & bc.code ? '1' : '0';
+    mask = mask << 1;
+  }
+  str[bc.len] = '\0';
+  printf("%s", str);
+}
+
+bit_code_t bit_code_empty(){
+  bit_code_t bc;
+  bc.len = 0;
+  bc.code = 0;
+  return bc;
+}
+
+bit_code_t bit_code_add_one(bit_code_t bc){
+  bit_code_t nbc = bc;
+  nbc.code = nbc.code | (1u << nbc.len);
+  nbc.len++;
+  return nbc;
+}
+
+bit_code_t bit_code_add_zero(bit_code_t bc){
+  bit_code_t nbc = bc;
+  nbc.len++;
+  return nbc;
+}
 
 typedef struct byte_freq_t{
   unsigned char byte;
@@ -30,6 +54,44 @@ typedef struct node_t{
   struct node_t* left;
   struct node_t* right;
 } node_t;
+
+typedef struct node_queue_t{
+  node_t** nodes;
+} node_queue_t;
+
+node_queue_t* node_queue_create(size_t size){
+  node_queue_t* nq = malloc(sizeof(node_queue_t));
+  cvector_vector_type(node_t*) nodes = NULL;
+  cvector_reserve(nodes, size);
+  nq->nodes = nodes;
+  return nq;
+}
+
+size_t node_queue_size(node_queue_t* nq){
+  return cvector_size(nq->nodes);
+}
+
+void node_queue_insert(node_queue_t* nq, node_t* node){
+  cvector_push_back(nq->nodes, node); 
+}
+
+node_t* node_queue_pop(node_queue_t* nq){
+  node_t* node = nq->nodes[0];
+  int index = 0;
+  for(int i = 0; i < cvector_size(nq->nodes); i++){
+    if(nq->nodes[i]->freq < node->freq){
+      node = nq->nodes[i];
+      index = i;
+    }
+  }
+  cvector_erase(nq->nodes, index);
+  return node;
+}
+
+void node_queue_free(node_queue_t* nq){
+  cvector_free(nq);
+  free(nq);
+}
 
 int cmp_byte_freq (const void * a, const void * b) {
    return ( ((byte_freq_t*)a)->freq - ((byte_freq_t*)b)->freq );
@@ -104,82 +166,59 @@ unsigned char* get_input_buffer(const char* filename){
   return vec;
 }
 
-// node_t create_tree(uni_vec_t* bf_arr){
-//   uni_vec_t* nodes = uni_vec_create(bf_arr->size, sizeof(node_t));
+node_t* create_tree(byte_freq_t* bf_arr){
+  node_queue_t* node_queue = node_queue_create(cvector_size(bf_arr));
+
+  //create a leaf node for each symbol
+  for(int i = 0; i < cvector_size(bf_arr); i++){
+    node_t* node = malloc(sizeof(node_t));
+    *node = (node_t){
+      byte: bf_arr[i].byte,
+      freq: bf_arr[i].freq,
+      leaf: true
+    };
+    node_queue_insert(node_queue, node);
+  }
+
+  //while more than one node in the queue
+  while(node_queue_size(node_queue) > 1){
+    //remove two nodes of highest probability
+    node_t* nodeA = node_queue_pop(node_queue);
+    node_t* nodeB = node_queue_pop(node_queue);
+    //create new internal node
+    node_t* node_internal = malloc(sizeof(node_t));
+    *node_internal = (node_t){
+      freq: nodeA->freq + nodeB->freq,
+      leaf: false,
+      left: nodeA,
+      right: nodeB
+    };
+    node_queue_insert(node_queue, node_internal);
+  }
   
-//   while(bf_arr->size > 0){
-//     byte_freq_t bf;
-//     uni_vec_extract(bf_arr, 0, &bf);
-    
-//     if(nodes->size < 2){//create leaf node with byte 
-//       node_t node = (node_t){
-//         byte: bf.byte, 
-//         freq: bf.freq,
-//         leaf: true,
-//         left: NULL,
-//         right: NULL};
-//       uni_vec_push_back(nodes, &node);
-//       // printf("new leaf: %c %i \n", node.byte, node.freq);
-//       uni_vec_pop_front(bf_arr);
-//     }else{
-//      node_t* first_node = (node_t*)uni_vec_get(nodes, 0);      
-//      node_t* second_node = (node_t*)uni_vec_get(nodes, 1);
-//      if(first_node->freq + second_node->freq < bf.freq){//connect first 2 nodes
-//       node_t node = (node_t){
-//         byte: 0, 
-//         freq: first_node->freq + second_node->freq,
-//         leaf: false,
-//         left: first_node,
-//         right: second_node};
-//       uni_vec_pop_front(nodes);
-//       uni_vec_pop_front(nodes);
-//       uni_vec_push_front(nodes, &node);
-//       // printf("new root: %c %i \n", node.byte, node.freq);
-//      }else{//add another leaf node
-//       node_t node = (node_t){
-//         byte: bf.byte, 
-//         freq: bf.freq,
-//         leaf: true,
-//         left: NULL,
-//         right: NULL};
+  node_t* root = node_queue_pop(node_queue);
+  // node_queue_free(node_queue);
+  return root;
+}
 
-//       // printf("new leaf: %c %i \n", node.byte, node.freq);
-//       uni_vec_push_back(nodes, &node);
-//       uni_vec_pop_front(bf_arr);
-//      }
-//     }
-//   }
-
-//   while(nodes->size != 1){
-//     node_t* first_node = (node_t*)uni_vec_get(nodes, 0);      
-//     node_t* second_node = (node_t*)uni_vec_get(nodes, 1);
-//     node_t node = (node_t){
-//         byte: 0, 
-//         freq: first_node->freq + second_node->freq,
-//         leaf: false,
-//         left: first_node,
-//         right: second_node};
-//       uni_vec_pop_front(nodes);
-//       uni_vec_pop_front(nodes);
-//       uni_vec_push_front(nodes, &node);
-//       // printf("new root: %c %i \n", node.byte, node.freq);
-//   }
-
-//   node_t root;
-//   uni_vec_extract(nodes, 0, &root);
-//   uni_vec_free(nodes);
-
-//   return root;
-// }
-
-void tree_traversal(node_t* node, unsigned char code){
+void tree_traversal(node_t* node, bit_code_t bc){
   if(node == NULL) return;
   if(node->leaf){
-    printf("%c: "BYTE_TO_BINARY_PATTERN"\n", node->byte, BYTE_TO_BINARY(5));
+    printf("%c: ", node->byte);
+    bit_code_print(bc);
+    printf("\n");
   }else{
-    tree_traversal(node->left, (code << 1) & 0);
-    tree_traversal(node->right, (code << 1) & 1);
+    tree_traversal(node->left, bit_code_add_zero(bc));
+    
+    tree_traversal(node->right, bit_code_add_one(bc));
   }
+}
+
+void tree_free(node_t* node){
+  if(node == NULL) return;
+  tree_free(node->left);
+  tree_free(node->right);
+  free(node);
 }
 
 int main(int argc, char** argv){
@@ -191,14 +230,15 @@ int main(int argc, char** argv){
   unsigned char* buffer = get_input_buffer(argv[1]); 
   byte_freq_t* bf_arr = get_byte_freq_arr(buffer);
 
-  for(int i = 0; i < cvector_size(bf_arr); i++){
-    byte_freq_t bf = bf_arr[i];
-    printf("%c : %d\n", bf.byte, bf.freq);
-  }
+  // for(int i = 0; i < cvector_size(bf_arr); i++){
+  //   byte_freq_t bf = bf_arr[i];
+  //   printf("%c : %d\n", bf.byte, bf.freq);
+  // }
 
-  // node_t root = create_tree(bf_arr);
-
-  // tree_traversal(&root, 0);
+  // node_t* root = create_tree(bf_arr);
+  // bit_code_t bc = bit_code_empty();
+  // tree_traversal(root, bc);
+  // tree_free(root);
 
   cvector_free(buffer);
   cvector_free(bf_arr);
