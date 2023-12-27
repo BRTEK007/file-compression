6c230d624f676a88a99bc7f9cb6a8c407a82d686
BRTEK007 <bartoszkonderla@gmail.com>
2023-12-28 00:01:46 +0100

    no logs

--- src/compress.cpp
+++ src/compress.cpp
@@ -6,8 +6,6 @@
 #include "bit_code.hpp"
 #include "bit_ostream.hpp"
 
-#include <iostream>
-
 inline void streamToVector(std::istream &inStream, std::vector<unsigned char> &outVector)
 {
   inStream.seekg(0, std::ios::end);
@@ -22,41 +20,19 @@ inline void streamToVector(std::istream &inStream, std::vector<unsigned char> &o
 void compress(std::istream &inStream, std::ostream &outStream)
 {
   std::vector<unsigned char> inBuffer;
-
   streamToVector(inStream, inBuffer);
-  // TODO inStream to inBuffer
 
   auto byteFreqArr = findByteFrequencies(inBuffer);
-
-  uint16_t uniqueByteCount = byteFreqArr.size();
 
   auto tree = Tree(byteFreqArr);
 
   std::array<BitCode, 256> codes;
-
   tree.extractCodes(codes);
-
-  auto totalByteCount = inBuffer.size();
-  printf("-------------------------\n");
-  printf("COMPRESSING %ld BYTES, %d UNIQUE\n", totalByteCount, uniqueByteCount);
-  printf("-------------------------\n");
-  printf("BYTE   | FREQUENCY | CODE\n");
-  printf("-------------------------\n");
-  for (int i = 0; i < uniqueByteCount; i++)
-  {
-    ByteFreq bf = byteFreqArr[i];
-    if (std::isprint(bf.byte))
-      printf("%d (%c) | %9.1f | ", bf.byte, bf.byte, (float)(100 * bf.freq) / totalByteCount);
-    else
-      printf("%d | %9.1f | ", bf.byte, (float)(100 * bf.freq) / totalByteCount);
-    std::cout << codes[bf.byte].to_string();
-    printf("\n");
-  }
-  printf("-------------------------\n");
 
   auto bitOstream = BitOstream(outStream);
 
   //  write 2 bytes -> unique bytes count
+  uint16_t uniqueByteCount = byteFreqArr.size();
   unsigned char *bytes = reinterpret_cast<unsigned char *>(&uniqueByteCount);
   bitOstream.write(bytes[0]);
   bitOstream.write(bytes[1]);
@@ -69,10 +45,4 @@ void compress(std::istream &inStream, std::ostream &outStream)
     bitOstream.write(code);
   }
   bitOstream.flush();
-
-  // extract to out_buffer from bitOutSream
-
-  // double reduction = 100.f * (1.f - ((double)out_buffer.size() / (double)inBuffer.size()));
-  // printf("size after compression: %ld, %.1f%% reduction\n", out_buffer.size(), reduction);
-  // printf("-------------------------\n");
 }
--- src/decompress.cpp
+++ src/decompress.cpp
@@ -3,42 +3,24 @@
 #include "tree.hpp"
 #include "bit_istream.hpp"
 
-#include <iostream>
-
 void decompress(std::istream &inStream, std::ostream &outStream)
 {
-  // create BitIstream from in_buffer
   auto bitIstream = BitIstream(inStream);
 
-  // read 4 bytes -> total_characters count
+  // read 2 bytes -> unique bytes count
   unsigned char bytes[2];
-  //  read 1 byte -> unique bytes count
   bytes[0] = bitIstream.readByte(); // TODO possible error/ might file be too small
   bytes[1] = bitIstream.readByte();
   uint16_t uniqueByteCount = *(reinterpret_cast<uint16_t *>(bytes));
-  //
-  printf("-------------------------\n");
-  printf("DECOMPRESSING %d UNIQUE\n", uniqueByteCount);
-  printf("-------------------------\n");
-  // read huffman tree data
+
   Tree tree;
   tree.readFrom(bitIstream, uniqueByteCount); // TODO possible error
+
   std::vector<unsigned char> leafBytes;
   tree.extractLeafBytes(leafBytes);
 
   std::array<BitCode, 256> codes;
-
   tree.extractCodes(codes);
-
-  printf("BYTE   | CODE\n");
-  printf("-----------\n");
-  for (size_t i = 0; i < leafBytes.size(); i++)
-  {
-    printf("%d (%c) | ", leafBytes[i], leafBytes[i]);
-    std::cout << codes[leafBytes[i]].to_string();
-    printf("\n");
-  }
-  printf("-------------------------\n");
 
   tree.ptrReset();
   while (!bitIstream.eof())
